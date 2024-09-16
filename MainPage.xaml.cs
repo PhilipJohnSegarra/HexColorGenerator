f00d022e4c0daec098ead4fe4b545d1896f0fa1e
philip <philipjohn.segarra@gmail.com>
2024-09-16 20:20:09 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Maui.Controls;
+namespace HexColorGenerator
+{
+    public partial class MainPage : ContentPage
+    {
+        string hexColor, red, green, blue;
+
+        private void ContentPage_Loaded(object sender, EventArgs e)
+        {
+            HexColor = Color.FromRgb((int)redSlider.Value, (int)greenSlider.Value, (int)blueSlider.Value);
+            witchImage.BackgroundColor = HexColor;
+            hexColor = HexColor.ToHex();
+            hexLabel.Text = hexColor;
+        }
+
+        private void copy_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        Color HexColor = Color.FromRgb(0,0,0);
+        
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            HexColor = Color.FromRgb((int)redSlider.Value, (int)greenSlider.Value, (int)blueSlider.Value);
+            witchImage.BackgroundColor = HexColor;
+
+            redLabel.Text = "  Red: " + (int)redSlider.Value;
+            greenLabel.Text = "  Green: " + (int)(greenSlider.Value);
+            blueLabel.Text = "  Blue: " + (int)(blueSlider.Value);
+
+            hexColor = HexColor.ToHex();
+            hexLabel.Text = hexColor;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            witchImage.BackgroundColor = HexColor;
+        }
+    }
+
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace HexColorGenerator
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+            // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
